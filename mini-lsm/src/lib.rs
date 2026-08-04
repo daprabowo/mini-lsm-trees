795f7431c5a42a93333a7c0f3d399f15cbec62ef
Dimas Prabowo <dimasprabowo98@icloud.com>
2026-08-05 03:55:06 +0700

    feat: create mem-table

--- mini-lsm/src/lib.rs
+++ mini-lsm/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod block;
 pub mod iterators;
 pub mod key;
+pub mod mem_table;
 pub mod wal;
---
+++ mini-lsm/src/mem_table.rs
@@ -0,0 +1,149 @@
+use std::{
+    ops::Bound,
+    path::Path,
+    sync::{Arc, atomic::AtomicUsize},
+};
+
+use anyhow::Result;
+use bytes::Bytes;
+use crossbeam_skiplist::SkipMap;
+use ouroboros::self_referencing;
+
+use crate::{iterators::StorageIterator, key::KeySlice, wal::Wal};
+
+/// A basic mem-table based on crossbeam-skiplist.
+pub struct MemTable {
+    map: Arc<SkipMap<Bytes, Bytes>>,
+    wal: Option<Wal>,
+    id: usize,
+    approximate_size: Arc<AtomicUsize>,
+}
+
+/// Create a bound of `Bytes` from a bound of `&[u8]`.
+pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
+    match bound {
+        Bound::Included(x) => Bound::Included(Bytes::copy_from_slice(x)),
+        Bound::Excluded(x) => Bound::Excluded(Bytes::copy_from_slice(x)),
+        Bound::Unbounded => Bound::Unbounded,
+    }
+}
+
+impl MemTable {
+    /// Create a new mem-table
+    pub fn create(_id: usize) -> Self {
+        unimplemented!()
+    }
+
+    /// Create a new mem-table with WAL
+    pub fn create_with_wal<P>(_id: usize, _path: P) -> Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        unimplemented!()
+    }
+
+    /// Create a mem-table with WAL
+    pub fn recover_from_wal<P>(_id: usize, _path: P) -> Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        unimplemented!()
+    }
+
+    pub fn for_testing_put_slice(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        self.put(key, value)
+    }
+
+    pub fn for_testing_get_slice(&self, key: &[u8]) -> Option<Bytes> {
+        self.get(key)
+    }
+
+    pub fn for_testing_scan_slice(
+        &self,
+        lower: Bound<&[u8]>,
+        upper: Bound<&[u8]>,
+    ) -> MemTableIterator {
+        self.scan(lower, upper)
+    }
+
+    /// Get a value by key.
+    pub fn get(&self, key: &[u8]) -> Option<Bytes> {
+        unimplemented!()
+    }
+
+    /// Put a key-value pair into the mem-table
+    pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub fn put_batch(&self, data: &[(KeySlice, &[u8])]) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub fn sync_wal(&self) -> Result<()> {
+        if let Some(ref wal) = self.wal {
+            wal.sync()?;
+        }
+        Ok(())
+    }
+
+    /// Get an iterator over a range of keys.
+    pub fn scan(&self, lower: Bound<&[u8]>, upper: Bound<&[u8]>) -> MemTableIterator {
+        unimplemented!()
+    }
+
+    // /// Flush the mem-table to SSTable.
+    // pub fn flush(&self, builder: &mut SsTableBuilder) -> Result<()> {
+    //     unimplemented!()
+    // }
+
+    pub fn id(&self) -> usize {
+        self.id
+    }
+
+    pub fn approximate_size(&self) -> usize {
+        self.approximate_size
+            .load(std::sync::atomic::Ordering::Relaxed)
+    }
+
+    /// Only use this function when closing the database
+    pub fn is_empty(&self) -> bool {
+        self.map.is_empty()
+    }
+}
+
+type SkipMapRangeIter<'a> =
+    crossbeam_skiplist::map::Range<'a, Bytes, (Bound<Bytes>, Bound<Bytes>), Bytes, Bytes>;
+
+/// An iterator over a range of `SkipMap`.
+#[self_referencing]
+pub struct MemTableIterator {
+    /// Stores a referene to the skipmap.
+    map: Arc<SkipMap<Bytes, Bytes>>,
+    /// Stores a skipmap iterator that refers to the lifetime of `MemTableIterator` itself.
+    #[borrows(map)]
+    #[not_covariant]
+    iter: SkipMapRangeIter<'this>,
+    /// Stores the current key-value pair.
+    item: (Bytes, Bytes),
+}
+
+impl StorageIterator for MemTableIterator {
+    type KeyType<'a> = KeySlice<'a>;
+
+    fn value(&self) -> &[u8] {
+        unimplemented!()
+    }
+
+    fn key(&self) -> Self::KeyType<'_> {
+        unimplemented!()
+    }
+
+    fn is_valid(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn next(&self) -> Result<()> {
+        unimplemented!()
+    }
+}
