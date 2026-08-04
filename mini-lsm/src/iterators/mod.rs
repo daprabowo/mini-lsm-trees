96a121b54e6592f5a90b94bcf21a91d0bf5238ac
Dimas Prabowo <dimasprabowo98@icloud.com>
2026-08-05 03:53:38 +0700

    feat: create storage iterator

---
+++ mini-lsm/src/iterators/mod.rs
@@ -0,0 +1,23 @@
+
+pub trait StorageIterator {
+    type KeyType<'a>: PartialEq + Eq + PartialOrd + Ord
+    where
+        Self: 'a;
+
+    /// Get the current value.
+    fn value(&self) -> &[u8];
+
+    /// Get the current value.
+    fn key(&self) -> Self::KeyType<'_>;
+
+    /// Check if the current iterator is valid
+    fn is_valid(&self) -> bool;
+
+    /// Move to the next position.
+    fn next(&self) -> anyhow::Result<()>;
+
+    /// Number of underlying active iterators for this iterator.
+    fn num_active_iterators(&self) -> usize {
+        1
+    }
+}
--- mini-lsm/src/lib.rs
+++ mini-lsm/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod block;
+pub mod iterators;
 pub mod key;
